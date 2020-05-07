9d297d1b8e068479d7207df2749b1d291edb25ee
tim.chen <a794281961@126.com>
2020-05-07 22:15:35 +0800

    增加unittest断言方法

--- functional_tests.py
+++ functional_tests.py
@@ -7,11 +7,26 @@
 # @Email   : 794281961@qq.com
 
 from selenium import webdriver
-import pytest
-
-br = webdriver.Chrome()
-br.get("http://localhost:8000")
-assert 'Django' in br.title
-br.quit()
+import unittest
 
 
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.br = webdriver.Chrome()
+        # 隐式等待3s
+        self.br.implicitly_wait(3)
+
+    def tearDown(self):
+        self.br.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 伊迪丝听说有一个很酷的在线待办事项应用
+        # 她去看了这个应用的首页
+        self.br.get("http://localhost:8000")
+        # 她注意到网页的标题和头部都包含“To-Do”这个词
+        self.assertIn('To-Do', self.br.title)
+        self.fail("Finish the test!")
+
+
+if __name__ == '__main__':
+    unittest.main(warnings="ignore")
